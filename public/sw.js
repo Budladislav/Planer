08d48e4b3b5f570567ba49ba77e356de9b58d32c
Budladislav <budladislav@gmail.com>
2026-09-02 22:27:22 +0300

    chore: prepare MonoFocus 3.5.0

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
 // Service Worker для MonoFocus Planner
 // Версия кэша - обновлять при изменении статики
-const STATIC_CACHE = 'monofocus-static-v3.4.1';
+const STATIC_CACHE = 'monofocus-static-v3.5.0';
 
 // Файлы для кэширования (статичные ресурсы)
 // Пути должны соответствовать base path из vite.config.ts
